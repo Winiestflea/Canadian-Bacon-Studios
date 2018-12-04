ce68e2e66c099f3afe6c19712cdd9d581063c62d
Winiestflea <bme2589@gmail.com>
2018-12-04 17:29:26 -0600

    Class Programs
    Tests

---
+++ Programming Learning/Assets/Scripts/Keyboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour {
+
+
+    public bool ShowHello;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ShowHello)
+            Debug.Log(Input.GetKey(KeyCode.A));
+        }
+	}
+}
--- Programming Learning/Assets/Scripts/Sample.cs
+++ Programming Learning/Assets/Scripts/Sample.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Sample : MonoBehaviour {
 
+    public string MyName;
+    public int MyNumber;
+    public long MyBigLong;
+    public float MyDecimal;
+    public bool ShowHello;
+    public double MyDoubleDecimal;
+    public char MyCharacter;
+
+    void Run()
+    {
+        Debug.Log("Run, "+MyName+"!");
+        Debug.Log(MyName + ", " + MyNumber + ", " + MyBigLong + ", " + MyDecimal + ", " + ShowHello + ", " + MyDoubleDecimal + ", " + MyCharacter);
+    }
+
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Hello World!");
+        if (MyNumber != 10) {
+            string otherName = "Bob";
+            Debug.Log("Hello " + otherName + " and " + MyName);
+        }
+        else if (MyNumber > 5)
+            Debug.Log("No message to show");
+        else if (MyNumber == 2)
+            Debug.Log("end");
 	}
 	
-	// Update is called once per frame
+	/* Update is called once per frame
 	void Update () {
         Debug.Log("Hi");
 	}
+    */
 }
