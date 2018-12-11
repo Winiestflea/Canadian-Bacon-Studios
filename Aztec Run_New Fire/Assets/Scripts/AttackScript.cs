2207013eb9556f08061c79bd6ee8d4273eee27e4
Winiestflea <bme2589@gmail.com>
2018-12-11 16:52:35 -0600

    1.1.8
    - Added Framework for the Attack.

---
+++ Aztec Run_New Fire/Assets/Scripts/AttackScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour {
+
+    private bool isActive = false;
+    private bool pressed = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && pressed == false)
+        {
+            isActive = true;
+            pressed = true;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && pressed == true)
+        {
+            isActive = false;
+            pressed = false;
+        }
+    }
+}
