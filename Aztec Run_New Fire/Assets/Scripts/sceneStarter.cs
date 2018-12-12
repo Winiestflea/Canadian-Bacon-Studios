501ff538a6c331e29432d3cf11c4cd896c9e76ba
Alex <alexplays003@gmail.com>
2018-12-12 14:59:30 -0600

    1.2.4
    fixed wierd script glitches

--- Aztec Run_New Fire/Assets/Scripts/sceneStarter.cs
+++ Aztec Run_New Fire/Assets/Scripts/sceneStarter.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class sceneStarter : MonoBehaviour {
+
     public Transform player;
     public Transform camara;
     public cameraFollower camScript;
     public playerController playerScripts;
     public dedificatorHelper dedificator;
+    // this canvas group is for the opacity of the object
+    public CanvasGroup image;
+
+    //this is the duration time
+    public float fadeDur = 5f;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(fadeIn());
+        startup();
 	}
 	
-	// Update is called once per frame
+	/*
 	void Update () {
 		if (player.position.x < 0 )
         {
@@ -28,4 +36,32 @@ public class sceneStarter : MonoBehaviour {
             dedificator.isActive = true;
         }
 	}
+    */
+
+    void startup( )
+    {
+        while ( player.position.x < 0 )
+        {
+            player.position = new Vector3(player.position.x + 0.05f, player.position.y, player.position.z);
+        }
+        playerScripts.enabled = true;
+        camScript.isActive = true;
+        dedificator.isActive = true;
+    }
+
+    // this IEnumerator is the thing that gets called and starts working next to our code
+    IEnumerator fadeIn()
+    {
+        // this float tells us at what time we started
+        float start = Time.time;
+
+        while ( (Time.time - start) //we start taking the opacity away and count the time that has passed
+            < fadeDur ) // if the time passed is smaller, than the finish, just continue
+        {
+            image.alpha = Mathf.Lerp(1 // this one means 
+                , 0, ( Time.time - start ) / fadeDur); // take the alpha of our image away
+            yield return null; // This helps the function keep track of the time that it has
+        }
+        image.alpha = 0; // take out that little float remainder and just turn it of
+    }
 }
