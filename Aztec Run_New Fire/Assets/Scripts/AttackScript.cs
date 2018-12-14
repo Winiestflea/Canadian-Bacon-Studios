564401a39fbbd781d8ca2927732da138240701b5
Alex <alexplays003@gmail.com>
2018-12-13 22:50:54 -0600

    1.4.2
    explosion

--- Aztec Run_New Fire/Assets/Scripts/AttackScript.cs
+++ Aztec Run_New Fire/Assets/Scripts/AttackScript.cs
@@ -7,6 +7,7 @@ public class AttackScript : MonoBehaviour
 
     public bool isActive = false;
     public Collider2D existing;
+    public SpriteRenderer rendificator;
 
     void Update()
     {
@@ -23,13 +24,15 @@ public class AttackScript : MonoBehaviour
     {
         // do startup sequence
         existing.enabled = true;
+        rendificator.enabled = true;
         isActive = false;
 
         // wait for 1 sec(or more if needed later)
         yield return new WaitForSeconds(0.2f);
         // do endup sequence 
         existing.enabled = false;
-        
+        rendificator.enabled = false;
+
     }
 
 }
